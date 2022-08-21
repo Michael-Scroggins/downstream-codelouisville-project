cca653ef9313257e18309bf02287002ba1f13ced
Michael-Scroggins <j.mikescroggins@gmail.com>
2022-08-21 17:31:18 -0400

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Downstream.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
